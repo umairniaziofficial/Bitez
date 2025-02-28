403bec1e12547ffecbed589894ede3eadcd2f747
Umair Niazi <umairniaziofficial@gmail.com>
2025-03-01 00:53:58 +0500

    Refactor Contact Us component and improve menu styling with responsive design

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
+  submitted = false;
 
+  onSubmit() {
+    this.submitted = true;
+  }
 }
